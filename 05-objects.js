d655ba4f0b87a1e1416e7915568835869416b7d4
Dennis Artsanyo <68733626+Artsanyo@users.noreply.github.com>
2023-03-15 21:02:37 +0200

    1

--- 05-objects.js
+++ 05-objects.js
@@ -1,8 +1,15 @@
 const cookie = {
     name: "Chocolate Chip",
     isGlutenFree: false,
-    origin: "Texas"
+    origin: "Texas",
+    "+Yummy": true
 };
 
+cookie.origin = "Kenya"
+
 console.log(cookie);
-console.log(cookie.name)
+console.log(cookie.name) // property of name
+console.log(cookie["name"]) // property of name
+//using square bracket to access object properties
+//using square bracket to access object properties  object["key"] or object.name
+console.log(cookie["+Yummy"]);
